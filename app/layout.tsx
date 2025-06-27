fc1c0e5a875514ad6887d9ea9a70e4c87783a3cb
Michael Reuter <mareuternh@gmail.com>
2025-06-27 13:49:42 -0700

    Adding nav bar.

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next'
 import { DateProvider } from '@/app/lib/date_context'
 import Loader from '@/app/ui/loader'
 import '@/app/globals.css'
+import NavBar from './ui/nav_bar'
 
 export const metadata: Metadata = {
   title: 'Lunar Club and Lunar II Information',
@@ -19,6 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <DateProvider>
+          <NavBar />
           <Suspense fallback={<Loader />}>{children}</Suspense>
         </DateProvider>
       </body>
